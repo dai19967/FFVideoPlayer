3b4cb078db3ca6aa91a58501ef9f0902f08d9909
增智 戴 <dai199574@outlook.com>
2019-09-17 22:01:09 +0800

    first commit

---
+++ myffmpeg.cpp
@@ -0,0 +1,6 @@
+#include "myffmpeg.h"
+
+MyFFmpeg::MyFFmpeg()
+{
+
+}
---
+++ myffmpeg.h
@@ -0,0 +1,16 @@
+#ifndef MYFFMPEG_H
+#define MYFFMPEG_H
+
+
+class MyFFmpeg : public QQuickItem
+{
+    Q_OBJECT
+public:
+    MyFFmpeg();
+
+signals:
+
+public slots:
+};
+
+#endif // MYFFMPEG_H
